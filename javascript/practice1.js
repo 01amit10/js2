c11e23f0a777dab9898b877c5f94dcb4c46dc4d8
01amit10 <143583753+01amit10@users.noreply.github.com>
2025-08-09 09:27:59 +0000

    updated

--- javascript/practice1.js
+++ javascript/practice1.js
@@ -1,1 +1,14 @@
-console.log("amit")
+// console.log("amit");
+// const name = "amit"
+// const age  = "21"
+// console.log(`my name is ${name} my age is ${age}`)
+
+// const gamename = new String('hjsjdj')
+// console.log(gamename.indexOf('j'))
+// const hun = 100000
+// console.log(hun.toLocaleString('en-IN'))
+// const h = new Number();
+// console.log(Number.MAX_VALUE)
+// console.log(Math.ceil(4.9999))
+let mydate = new Date()
+console.log(mydate.getMonth())
