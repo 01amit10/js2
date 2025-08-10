d1ab60366240dd9891fce8a752432d8447356db2
01amit10 <143583753+01amit10@users.noreply.github.com>
2025-08-10 08:17:43 +0000

    update2

--- javascript/practice1.js
+++ javascript/practice1.js
@@ -9,6 +9,38 @@
 // console.log(hun.toLocaleString('en-IN'))
 // const h = new Number();
 // console.log(Number.MAX_VALUE)
-// console.log(Math.ceil(4.9999))
-let mydate = new Date()
-console.log(mydate.getMonth())
+// // console.log(Math.ceil(4.9999))
+// let mydate = new Date()
+// console.log(mydate.getMonth())
+
+// const obj1 = {
+//     1:"a",2:"b"
+// }
+// const obj2 = {
+//     ar:"a",4:"b"
+// }
+// const obj3 = {
+//     5:"a",6:"b"
+// }
+// // const obj4 = Object.assign({},obj1,obj2,obj3)
+// const{ar:k} = obj2
+// console.log(k);
+
+// function user(username){
+//     return `${username} you have logged`
+// }
+// const s = user("amit")
+// console.group(s);
+
+// const add2 = (num1,num2)=>
+// {
+
+//     return num1+num2;
+// }
+// (()=>{
+//     console.log("ajas")
+// })();
+// (function a(){
+//     console.log("hi")
+// })()
+
